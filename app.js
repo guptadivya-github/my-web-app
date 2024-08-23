74c035f09a27c6a7389ab50ac7b3c3234715b30e
guptadivya-github <guptadivya1703@gmail.com>
2024-08-23 12:21:09 +0530

    Update app.js

--- app.js
+++ app.js
@@ -9,7 +9,7 @@ app.get('/', (req, res) => {
         <title>Version 1</title>
         <style>
           body {
-            background-color: blue;
+            background-color: green;
             color: white;
             font-size: 24px;
             display: flex;
